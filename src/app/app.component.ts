4c918ff3abe9a0f004383f96a80972d0bb81f350
lifaiho0916 <phoenixdev1223@gmail.com>
2024-08-05 11:09:41 +0800

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    HomeComponent
+    HomeComponent,
+    RouterModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
